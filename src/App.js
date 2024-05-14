e7993eca64944877d8e76f3340a73249c29ae014
ThinhHoang0108 <hoangxthinh@gmail.com>
2024-05-14 19:31:57 +0700

    asyn/await & ES6 module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Demo/Header";
 import HeaderRFC from "./component/Demo/HeaderRFC";
+import React, {handle, value} from "./react.js";
+
 
 function App() {
   return (
@@ -62,22 +64,109 @@ export default App;
 // console.log(d)
 // let user = {}
 // user.address ? alert(user.address.street) : alert(undefined)
-function handle(cb){
-  cb();
-}
-class Cat{
-  constructor(name, color, type){
-    this.name = name
-    this.color = color
-    this.type = type
-    this.meow = this.meow.bind(this)
-  }
-  meow(){
-    console.log(`${this.name} meow: meow meow meow`)
-  }
-  run(){
-    handle(this.meow)
-  }
-}
-let alex = new Cat('Alex', 'Yellow', 'Bengal')
-alex.run()
+// function handle(cb){
+//   cb();
+// }
+// class Cat{
+//   constructor(name, color, type){
+//     this.name = name
+//     this.color = color
+//     this.type = type
+//     this.meow = this.meow.bind(this)
+//   }
+//   meow(){
+//     console.log(`${this.name} meow: meow meow meow`)
+//   }
+//   run(){
+//     handle(this.meow)
+//   }
+// }
+// let alex = new Cat('Alex', 'Yellow', 'Bengal')
+// alex.run()
+//----------------------------------------------------------
+//Function
+// Mảng
+// var keywords = ["Khoa", "codelearn", "Học lập trình", "codelearn.io"];
+
+// // Lặp qua từng phần tử và xử lý trong hàm callback
+// keywords.forEach(function (eachName, index){
+//     console.log(index + 1 + ". " + eachName);
+// });
+//Currying la function ma return ve function
+// function findNumber(num){
+//   return function(func){
+//     const result = [];
+//     for(let i = 0; i <= num; i++){
+//       if(func(i)){
+//         result.push(i)
+//       }
+//     }
+//     return result
+//   }
+// }
+// const value = findNumber(10)(number => number % 2 == 0)
+// const newFunc = findNumber(10)
+// const value2 = newFunc(number => number % 2 == 0)
+// console.log(value2)
+//++++AROW FUNCTION
+// const findNumber = (num) => (func)  => {
+//   const result = [];
+//   for (let i = 0; i <= num; i++) {
+//     if (func(i)) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// };
+// const value = findNumber(10)(number => number % 2 == 0)
+// console.log(value)
+//--------------------------------------Promise va Async await
+
+// setTimeout(() => {
+//   console.log('Hello')
+// }, 0)
+// const pFunc = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Hello");
+//     }, 0);
+//   });
+
+// const value = pFunc()
+//   .then((value) => {
+//     console.log(value);
+//   })
+//   .catch((error) => {
+//     console.warn(error);
+//   })
+//   .finally(() => {
+//     console.log('Ket thuc roi')
+//   });
+//   console.log(value)
+/**
+ * Asyn / Await
+ * Await chi su dung duoc trong mot await function
+ * Chi su dung await voi promise
+ */
+// const p2 = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("hello");
+//     }, 0)
+//   })
+// const handle = async () => {
+//   try {
+//     const value = await p2();
+//     console.log(value);
+//   } catch (error) {
+//     console.warn(error);
+//   } finally {
+//     console.log("Finally");
+//   }
+// }
+// handle()
+/**
+ * ES6 module
+ */
+console.log(value)
+handle()
---
+++ src/react.js
@@ -0,0 +1,7 @@
+const React = 'React'
+const handle = () => {
+    console.log('handle')
+}
+export const value = 100
+export {handle}
+export default React
