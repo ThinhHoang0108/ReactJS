d3daafea029b17203071d5ee1c470e361e738a3b
ThinhHoang0108 <hoangxthinh@gmail.com>
2024-07-29 22:47:42 +0700

    test vercel

--- src/component/Demo/Header.js
+++ src/component/Demo/Header.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-export default class Header extends Component { //React class component
-  render() { //render la phuong thuc hien thi noi dung cua component len giao dien nguoi dung
+export default class Header extends Component {
+  //React class component
+  render() {
+    //render la phuong thuc hien thi noi dung cua component len giao dien nguoi dung
     return (
       <div>
-       <h1>Hello header component</h1> 
+        <h1>Hello header component vercel test</h1>
       </div>
     )
   }
+}
+
+function Header2() {
+  const a = 10
 }
